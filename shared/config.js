e1b0222090b0eed51704e9e25f41252a779ff7a6
zAhir <md.zahir25@gmail.com>
2021-07-28 15:22:13 +0600

    responsive layout for mobile app

--- shared/config.js
+++ shared/config.js
@@ -2,5 +2,4 @@ module.exports = {
     PORT: 8080,
     URL: 'http://localhost:8080',
     MONGODB_URI: 'mongodb://localhost/cerebrum',
-    // MONGODB_URI: 'mongodb+srv://zahir:zahir123@zcluster-gw4at.mongodb.net/cerebrum?retryWrites=true&w=majority'
 };
